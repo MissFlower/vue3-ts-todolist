b4c356ebc3ba95de150e7e12f9755892429686a4
DoveyLoveyCora <DoveyLoveyCora@163.com>
2022-02-10 18:24:29 +0800

    ci: husky配置

---
+++ scripts/verify-commit.js
@@ -0,0 +1,19 @@
+const chalk = require('chalk')
+const msg = require('fs')
+	.readFileSync(`${process.env.INIT_CWD}/.git/COMMIT_EDITMSG`, 'utf-8')
+	.trim()
+
+const commitRE =
+	/^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
+
+if (!commitRE.test(msg)) {
+	console.error(
+		`  ${chalk.bgRed.white(' 错误 ')} ${chalk.red(`无效的提交消息格式。`)}\n\n` +
+			chalk.red(`  自动生成更改日志需要正确的提交消息格式。例子:\n\n`) +
+			`    ${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
+			`    ${chalk.green(`fix(v-model): handle events on blur (close #28)`)}\n\n` +
+			chalk.red(`  See .github/commit-convention.md for more details.\n`)
+	)
+	process.exit(1)
+}
+console.log(chalk.blue(`${msg}\n`) + chalk.green('commit信息格式正确，给予通过！'))
